1ad90f4b90beb3ddb40a8c381de0ddef09a903fa
Mateus <mateus.naxx@gmail.com>
2025-02-24 19:50:35 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,9 +1,8 @@
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
+
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/hearder";
 
 interface RestaurantMenuPageProps {
@@ -25,13 +24,17 @@ const RestaurantMenuPage = async ({
     if (!isConsumptionMethodValid(consumptionMethod)) {
         return notFound();
     }
-    const restaurant = await db.restaurant.findUnique({ where: { slug } });
+    const restaurant = await db.restaurant.findUnique({
+        where: { slug },
+        include: { menuCategories: { include: { products: true } } },
+    });
     if (!restaurant) {
         return notFound();
     }
     return (
         <div>
-            <RestaurantHeader restaurant= {restaurant}/>
+            <RestaurantHeader restaurant={restaurant} />
+            <RestaurantCategories restaurant={restaurant} />
         </div>
     );
 };
